ad684b255cb6cec59c576d05f7b6f32b5b842b0a
sandeshbharankar <242745132+sandeshbharankar@users.noreply.github.com>
2026-08-21 23:54:03 +0530

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        vector<pair<int, double>> cars;
+        for(int i=0;i<position.size();i++){
+            double time= (double)(target-position[i])/speed[i];
+            cars.push_back({position[i], time});
+        }
+        //sorting an position from nearest to farthest car from destinatino 
+
+        sort(cars.rbegin(), cars.rend());
+
+        //greedy technique
+        double last_time = 0;
+        int fleet =0;
+       
+       for(auto& car:cars){
+        double current_time= car.second;
+        if( current_time > last_time){
+        fleet++;
+        last_time= current_time;
+      
+        }
+          
+       }
+       return fleet;
+        
+    }
+};

9822c121a25422591a15b1df11a2c364b949af30
sandeshbharankar <242745132+sandeshbharankar@users.noreply.github.com>
2026-08-28 23:22:05 +0530

    Add: max-water-container - submission-21

---
+++ Data Structures & Algorithms/max-water-container/submission-21.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int l = 0, r = h.size() - 1;
+        long long ans = 0;
+
+        while (l < r) {
+            long long area = 1LL * min(h[l], h[r]) * (r - l);
+            ans = max(ans, area);
+
+            if (h[l] <= h[r])
+                l++;
+            else
+                r--;
+        }
+
+        return (int)ans;
+    }
+};
